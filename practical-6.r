8e978871bb501965f9312a52a52c84107fe197cb
benjaminturnbull01 <benorule@outlook.com>
2026-09-14 12:24:56 +0930

    created boxplot for age and survival

--- practical-6.r
+++ practical-6.r
@@ -1,4 +1,5 @@
 library(dplyr)
+library(ggplot2)
 titanic <- read_csv("titanic.csv")
 titanic <- titanic |>
 mutate(survived = as.logical(survived))
@@ -6,3 +7,9 @@ mutate(survived = as.logical(survived))
 titanic <- titanic |>
   mutate(accompanied = (sibsp + parch) > 0)
 
+ggplot(titanic, aes(x = survived, y = age)) +
+  geom_boxplot() +
+  labs(
+    x = "Survived",
+    y = "Age"
+  )
