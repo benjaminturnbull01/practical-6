aa5218900b072b1fd55abc51aaa802b64fafec9e
benjaminturnbull01 <benorule@outlook.com>
2026-09-14 12:20:19 +0930

    created accompanied variable

--- practical-6.r
+++ practical-6.r
@@ -2,3 +2,7 @@ library(dplyr)
 titanic <- read_csv("titanic.csv")
 titanic <- titanic |>
 mutate(survived = as.logical(survived))
+
+titanic <- titanic |>
+  mutate(accompanied = (sibsp + parch) > 0)
+
